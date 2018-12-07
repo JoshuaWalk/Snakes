a256c1b206fcdce48f5cc6d9884432dd2b084d01
Unknown <joshuassignup@gmail.com>
2018-12-07 14:28:52 -0700

    components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './TicTac'
+import Game from './components/TicTac'
 
 
 ReactDOM.render(<Game />, document.getElementById('root'));
