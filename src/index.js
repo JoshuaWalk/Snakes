ca394b157823b6a96d267986f9d9b0cc686b7dd4
Unknown <joshuassignup@gmail.com>
2018-12-17 14:29:02 -0700

    Put everything back also made it responsive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './components/TicTac'
+import App from './App'
 
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
